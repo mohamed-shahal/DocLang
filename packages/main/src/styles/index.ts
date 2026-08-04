958ede812405fb9ddefea79abab97003a56fa49e
shahal <workshahal@gmail.com>
2026-08-04 22:28:36 +0530

    feat: left-align default design and photo/address paragraphs

--- packages/main/src/styles/index.ts
+++ packages/main/src/styles/index.ts
@@ -11,7 +11,6 @@ export const defaultStyles: ResumeStyles = {
     bold: true,
     color: "000000",
     spacing: { before: 0, after: 50, line: 276 },
-    alignment: "center",
   },
   designation: {
     font: "Arial",
@@ -19,14 +18,12 @@ export const defaultStyles: ResumeStyles = {
     bold: false,
     color: "000000",
     spacing: { before: 0, after: 65, line: 276 },
-    alignment: "center",
   },
   contact: {
     font: "Arial",
     size: 17,
     color: "000000",
     spacing: { before: 0, after: 0, line: 240 },
-    alignment: "center",
   },
   sectionHeading: {
     font: "Arial",
@@ -81,7 +78,6 @@ export const defaultStyles: ResumeStyles = {
     bold: true,
     color: "000000",
     spacing: { before: 0, after: 60, line: 276 },
-    alignment: "center",
   },
   subHeading: {
     font: "Arial",
